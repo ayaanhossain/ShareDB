9a45e9e37aee84ecf0a76ba9016008e905124cc3
Ayaan Hossain <ain.hoss07@gmail.com>
2020-02-17 15:37:06 -0500

    quick benchmark example

--- examples/quick_bench.py
+++ examples/quick_bench.py
@@ -1,116 +1,51 @@
-import random
-import time
-import multiprocessing
-
 from ShareDB import ShareDB
 
-def get_string(length):
-    return ''.join(random.choice('ATGC') for _ in xrange(length))
+import time
 
-def populate_dict(dictionary, items):
-    population = []
-    total_time = 0.
-    # local_cach = {}
-    for i in xrange(items):
-        item = get_string(length=25)
-        t0 = time.time()
-        population.append(item)
-        # dictionary[item] = [random.randint(1, 4000),
-        #                     random.randint(1, 4000),
-        #                     random.randint(1, 4000)]
-        dictionary[item] = 0
-        total_time += time.time() - t0
-        # if len(local_cach) == 10**6:
-        #     dictionary.multiset(local_cach.iteritems())
-        #     local_cach = {}
-        if random.random() < 0.001:
-            print 'WRITER - throughput @ {} writes/second | Filled {}'.format(i / total_time, len(dictionary))
-    return population
 
-def worker_process(path, population, pid):
-    print 'ENTERED PID {} .. will PROCESS {} items'.format(pid, len(population))
-    dictionary = ShareDB(path='./test.ShareDB', reset=False)
-    # dictionary = LevelDict(path='./test.ShareDB')
-    t0 = time.time()
-    total_time = 0.
-    # random.shuffle(population)
-    for i, item in enumerate(population):
-        t0 = time.time()
-        # assert item in dictionary
-        val = dictionary[item]
-        # assert isinstance(val, int)
-        # assert val == 0
-        # dictionary[item] = val + 1
-        # dictionary.sync()
+def write_thp(store_path, num_items, length):
+    '''
+    Fill store with random DNA strings and report throughput.
+    '''
+    kvStore = ShareDB(
+        store_path,
+        True,
+        'msgpack', 
+        map_size=num_items*100)
+    i  = 1.
+    tt = 0.0
+    while i <= num_items:
+        t0  = time.time()
+        kvStore[i] = 1
+        tt += time.time() - t0
+        print('WRITER thp @ {:.2f} wt/sec | FILL {:.2f}%'.format(
+            i / tt, (100. * i) / num_items))
+        i += 1
 
-        # assert dictionary[item] == val+1
-        total_time += time.time() - t0
-        if random.random() < 0.001:
-            print 'READER {} - throughput @ {} queries/second | Remaining {}'.format(pid, i / total_time, len(dictionary) - i - 1)
-            # time.sleep(0.01)
+def read_thp(store_path):
+    '''
+    Go through store and report reading throughput.
+    '''
+    kvStore = ShareDB(store_path)
+    i  = 1.
+    tt = 0.0
+    while i <= len(kvStore):
+        t0  = time.time()
+        val = kvStore[i]
+        tt += time.time() - t0
+        print('READER thp @ {:.2f} rd/sec | FILL {:.2f}%'.format(
+            i / tt, (100. * i) / len(kvStore)))
+        i += 1
 
 def main():
-    # vedis.Vedis('./test.ShareDB')
-    dictionary = ShareDB(path='./test.ShareDB', reset=True)
-    # print dir(dictionary.DB.sync)
-    # sys.exit(0)
-    # dictionary[['KEY']] = 'SOME_VALUE'
-    # dictionary['NAME']    = 'Ayaan'
-    # dictionary['SURNAME'] = 'Hossain'
-    # assert 'NAME' in dictionary
-    # assert dictionary['NAME'] == 'Ayaan'
-    # assert len(dictionary) == 2
-    # dictionary.remove('NAME')
-    # assert not 'NAME' in dictionary
-    # assert 'SURNAME' in dictionary
-    #
-    # print dictionary.BUFFER
-    population = populate_dict(dictionary=dictionary, items=1000000)
-    # print len(dictionary.BUFFER)
-    # print len(dictionary)
-    dictionary.close()
-    del dictionary
-    dictionary = None
-    # # worker_process(dictionary, population, pid=0)
-    print '\n WRITE FINISHED \n'
+    store_path = './kvStore'
+    num_items  = 1000000
+    length     = 25
+    write_thp(store_path, num_items, length)
+    print('\n')
+    read_thp(store_path)
+    ShareDB(store_path).drop()
 
-    time.sleep(5)
-
-    # sys.exit(0)
-
-    # pool = multiprocessing.Pool(7)
-    # params = []
-    # for pid in xrange(7):
-    #     params.append((dictionary, population, pid))
-    # pool.map_async(worker_process, params)
-    # pool.close()
-
-    workers = []
-    for pid in xrange(7):
-        worker = multiprocessing.Process(
-            target=worker_process, args=('./test.ShareDB', population, pid, ))
-        workers.append(worker)
-    for worker in workers:
-        worker.start()
-    for worker in workers:
-        worker.join()
-
-    print '\n READ FINISHED \n'
-
-    dictionary = ShareDB(path='./test.ShareDB', reset=False)
-    dictionary.drop()
-
-    # dictionary = ShareDB(path='./test.ShareDB', reset=False)
-    # cnt = collections.defaultdict(int)
-    # pnt = 0
-    # for key in population:
-    #     if not key in dictionary:
-    #         pnt += 1
-    #     if dictionary[key] < 7:
-    #         cnt[dictionary[key]] += 1
-    # print cnt, pnt
-
-    # print '\n ANALYSIS FINISHED \n'
 
 if __name__ == '__main__':
     main()
